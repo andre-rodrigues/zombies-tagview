735ff9074f5378e8598d9eeec36c4f489f8d8fce
Rodrigo Navarro <rnavarro1@gmail.com>
2014-02-22 13:02:24 -0300

    Otimizando Rendering System para que só desenhe entidades que estão visíveis na tela.

--- src/systems/rendering_system.js
+++ src/systems/rendering_system.js
@@ -15,9 +15,17 @@
     process: function() {
       this.clear();
 
-      this.entities.toArray().sort(function(entityA, entityB) {
-        return entityA.spatial.y - entityB.spatial.y;
-      }).forEach(this.render, this);
+      var renderArea = this.world.screen;
+
+      this.entities
+          .toArray()
+          .filter(function(entity) {
+            return renderArea.intersects(entity.spatial);
+          })
+          .sort(function(entityA, entityB) {
+            return entityA.spatial.y - entityB.spatial.y;
+          })
+          .forEach(this.render, this);
     },
 
     clear: function() {
